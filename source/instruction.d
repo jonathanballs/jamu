e68a23224cc2254a002d8367a7f0813b86da4eb6
Jonathan Balls <jonathanballs@protonmail.com>
2018-02-27 03:55:37 +0000

    Printing for data processing instructions

--- source/instruction.d
+++ source/instruction.d
@@ -47,7 +47,27 @@ class Instruction {
             case 0b1110: return true;                           // AL
             default: assert(0); // Should throw invalid instruction
         }
-        return true;
+    }
+
+    string conditionString(uint cond) {
+        switch (cond) {
+            case 0b0000: return "EQ";
+            case 0b0001: return "NE";
+            case 0b0010: return "CS";
+            case 0b0011: return "CC";
+            case 0b0100: return "MI";
+            case 0b0101: return "PL";
+            case 0b0110: return "VS";
+            case 0b0111: return "VC";
+            case 0b1000: return "HI";
+            case 0b1001: return "LS";
+            case 0b1010: return "GE";
+            case 0b1011: return "LT";
+            case 0b1100: return "GT";
+            case 0b1101: return "LE";
+            case 0b1110: return "";
+            default: assert(0); // Should throw invalid instruction
+        }
     }
 
     Machine* execute(Machine *m) {
@@ -97,7 +117,6 @@ class BranchInstruction : Instruction {
 
     override Machine* execute(Machine *m) {
         import std.stdio;
-        writeln(m.pc(), " ", castedBytes.offset << 2);
         m.setRegister(15, m.pc() + (castedBytes.offset << 2) + 8);
         return m;
     }
@@ -105,15 +124,57 @@ class BranchInstruction : Instruction {
     override string toString() {
         auto mnemonic = "B" ~ (castedBytes.linkbit ? "L" : "");
         auto offset = castedBytes.offset << 2;
-        return format!"%s 0x%x"(mnemonic, offset + location + 8);
+        return format!"%s%s 0x%x"(mnemonic,
+               conditionString(castedBytes.cond),
+               offset + location + 8);
     }
 }
 
 class DataProcessingInstruction : Instruction {
-    this(uint location, ubyte[4] source) {
-        super(location, source);
-        //assert(this.castedBytes.opcode == 0b001);
+    struct DataProcessingInsn {
+        mixin(bitfields!(
+            uint, "operand2",    12,
+            uint, "destReg",    4,
+            uint, "operandReg", 4,
+            bool, "setBit",     1,
+            uint, "opcode",     4,
+            bool, "immediate",  1,
+            uint, "",           2,
+            uint, "cond",       4));
     }
 
+    string instructionString() {
+        switch (castedBytes.opcode) {
+            case 0b0000: return "AND";
+            case 0b0001: return "EOR";
+            case 0b0010: return "SUB";
+            case 0b0011: return "RSB";
+            case 0b0100: return "ADD";
+            case 0b0101: return "ADC";
+            case 0b0110: return "SBC";
+            case 0b0111: return "RSC";
+            case 0b1000: return "TST";
+            case 0b1001: return "TEQ";
+            case 0b1010: return "CMP";
+            case 0b1011: return "CMN";
+            case 0b1100: return "ORR";
+            case 0b1101: return "MOV";
+            case 0b1110: return "BIC";
+            case 0b1111: return "MVN";
+            default: assert(0); // Should throw invalid instruction
+        }
+    }
+
+    DataProcessingInsn* castedBytes() {
+        return cast(DataProcessingInsn *) source.ptr;
+    }
+
+    this(uint location, ubyte[4] source) {
+        super(location, source);
+    }
+
+    override string toString() {
+        return instructionString() ~ conditionString(castedBytes().cond);
+    }
 }
 
